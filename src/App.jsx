65a4b717038067ef17dc349abab3b1139a25c855
J-Breno <joao.breno85@hotmail.com>
2024-11-21 16:12:26 -0300

    Interando no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,65 @@
-import { Header } from "./components/Header"
+import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
-import './global.css';
-import styles from './App.module.css';
+import "./global.css";
+import styles from "./App.module.css";
 
-function App() {
+const post = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/J-Breno.png",
+      name: "Breno",
+      role: "Web Developer | Front-end",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2024-11-21 16:05:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/J-Breno.png",
+      name: "João",
+      role: "Web Developer | Back end",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2024-11-11 16:05:00"),
+  },
+];
 
+export function App() {
   return (
     <>
-     <Header />
-     <div className={styles.wrapper}>
-      <Sidebar />
-      <main>
-        <Post
-          author="Breno"
-          content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus voluptate, veritatis inventore dignissimos ipsa et perspiciatis reprehenderit quis quos incidunt similique soluta voluptatum dolor? Sint sequi delectus nam atque dolorem."
-        />
-        <Post
-          author="João"
-          content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus voluptate, veritatis inventore dignissimos."
-        />
-      </main>
-     </div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {post.map((post) => {
+            return (
+              <Post author={post.author} content={post.content} publishedAt={post.publishedAt}/>
+            );
+          })}
+        </main>
+      </div>
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
