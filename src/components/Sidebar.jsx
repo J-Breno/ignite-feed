96a81097be68571ae877fecb57ab7aa92b1bbf60
J-Breno <joao.breno85@hotmail.com>
2024-11-21 13:00:32 -0300

    Finalizando o sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from "./Sidebar.module.css";
+import { PencilLine } from 'phosphor-react';
 
 export function Sidebar() {
   return (
@@ -8,12 +9,13 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1666615435088-4865bf5ed3fd?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/J-Breno.png" />
         <strong>Breno</strong>
         <span>Web Developer</span>
       </div>
-      
+
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#"> <PencilLine size={20} /> Editar seu perfil</a>
       </footer>
     </aside>
   );
