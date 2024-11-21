afcacdf0451f04f22c1802bb5a7466ea40df9f96
J-Breno <joao.breno85@hotmail.com>
2024-11-21 12:05:20 -0300

    Component sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import { Header } from "./components/Header"
 import './global.css';
+import styles from './App.module.css';
+import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
 
   return (
     <>
      <Header />
+     <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+        <Post
+          author="Breno"
+          content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus voluptate, veritatis inventore dignissimos ipsa et perspiciatis reprehenderit quis quos incidunt similique soluta voluptatum dolor? Sint sequi delectus nam atque dolorem."
+        />
+        <Post
+          author="João"
+          content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus voluptate, veritatis inventore dignissimos."
+        />
+      </main>
+     </div>
     </>
   )
 }
+
 
 export default App
